f9466dba6c4de25500dc1e17160230352cf8b500
Vishalmohil63 <vmohil512@gmail.com>
2026-02-24 02:30:23 +0530

    Fixed axios baseURL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001"
+      : "https://chat-app-backend-irvu.onrender.com",
   withCredentials: true,
 });

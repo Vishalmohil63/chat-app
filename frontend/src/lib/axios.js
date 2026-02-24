15d976938ad7f5de9c6a9c9652dfdda2ef5e9ffc
Vishalmohil63 <vmohil512@gmail.com>
2026-02-24 11:07:13 +0530

    Fixed axios production config

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001"
-      : "https://chat-app-backend-irvu.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
